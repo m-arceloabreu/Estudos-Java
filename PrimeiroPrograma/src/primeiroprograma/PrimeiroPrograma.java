a403f815e00b3efbaedd313092dca54e43ff979d
Marcelo Abreu <marceloabreu1997@gmail.com>
2021-08-01 17:45:02 -0300

    Aulas Guanabara

---
+++ PrimeiroPrograma/src/primeiroprograma/PrimeiroPrograma.java
@@ -0,0 +1,21 @@
+
+package primeiroprograma;
+
+import java.util.Date;
+import java.util.Locale;
+
+
+public class PrimeiroPrograma {
+
+
+    public static void main(String[] args) {
+        System.out.println("Olá Mundo!");
+        Locale loc = Locale.getDefault();
+       
+    
+        System.out.println(loc.toString());
+        
+        
+    }
+    
+}
